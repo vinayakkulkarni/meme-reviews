2d06d4b68c418ba6200ceea443027c80f3d35277
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2017-12-13 16:08:24 +0530

    :8ball: Testing somethin' somethin' :tea:

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,29 @@
 <template>
-  <div id="app">
-    <header>
-      <span>Vue.js PWA</span>
-    </header>
-    <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
-      <router-view></router-view>
-    </main>
+  <div id="app" class="Site">
+    <section class="hero is-medium is-primary is-bold Site-content">
+      <div class="hero-body">
+        <div class="container is-fluid">
+          <div class="content has-text-centered">
+              <h1 class="title">
+              MEME 👏 REVIEWS
+            </h1>
+            <main>
+              <img src="./assets/logo.png" alt="MEME 👏 REVIEWS">
+              <router-view></router-view>
+            </main>
+          </div>
+        </div>
+      </div>
+    </section>
+    <footer class="footer" style="padding: 3rem 1.5rem;">
+      <div class="container">
+        <div class="content has-text-centered">
+          <p>
+            <strong>MEME 👏 REVIEWS.</strong> Made with ❤️
+          </p>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -15,40 +32,15 @@ export default {
   name: 'app',
 };
 </script>
-
-<style>
-body {
-  margin: 0;
+<style lang="sass" src="bulma"></style>
+<style scoped>
+.Site {
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
 }
 
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-
-main {
-  text-align: center;
-  margin-top: 40px;
-}
-
-header {
-  margin: 0;
-  height: 56px;
-  padding: 0 16px 0 24px;
-  background-color: #35495E;
-  color: #ffffff;
-}
-
-header span {
-  display: block;
-  position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: .02em;
-  font-weight: 400;
-  box-sizing: border-box;
-  padding-top: 16px;
+.Site-content {
+  flex: 1;
 }
 </style>
