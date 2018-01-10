4f55f483dc0cc5af47756ef68cc5d126bc4ffa8e
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2018-01-10 15:01:34 +0530

    Added a simple table - Work to be done here.

---
+++ src/components/Pages/Memes.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <table class="table">
+      <thead>
+        <tr>
+          <th><abbr title="Position">Pos</abbr></th>
+          <th>Meme</th>
+          <th>Rating by Pewds</th>
+          <th>Crowd rating</th>
+          <th>Rate</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th>1</th>
+          <td>ABC</td>
+          <td>38</td>
+          <td>23</td>
+          <td>23</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Memes',
+};
+</script>
