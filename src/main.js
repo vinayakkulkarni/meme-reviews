29d0ceb8df566c389ecc1fd4a2ef1c7c073d3b72
Vinayak <inbox.vinayak@gmail.com>
2018-02-17 23:55:29 +0530

    Added SweetAlert2

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import VueFire from 'vuefire';
+import VueSweetalert2 from 'vue-sweetalert2';
 import App from './App';
 import router from './router';
 
 require('../node_modules/font-awesome/css/font-awesome.css');
 
 Vue.use(VueFire);
+Vue.use(VueSweetalert2);
 
 Vue.config.productionTip = false;
 
